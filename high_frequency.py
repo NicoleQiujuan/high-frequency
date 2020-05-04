ec646b84cd1f73333a9ccd7a014681764704c66c
1bazinga25 <zyh_xg@163.com>
2020-05-04 10:20:17 +0800

    number format

--- high_frequency.py
+++ high_frequency.py
@@ -16,11 +16,11 @@ api_secret = config['api_secret']
 api_passphrase = config['api_passphrase']
 symbol_a = config['symbol_a']
 symbol_b = config['symbol_b']
-spread_mean = config['spread_mean']
-leverage = config['leverage']
-size = config['size']
+spread_mean = int(config['spread_mean'])
+leverage = int(config['leverage'])
+size = int(config['size'])
 sandbox = config['is_sandbox']
-num_param = config['num_param']
+num_param = int(config['num_param'])
 trade = Trade(api_key, api_secret, api_passphrase, is_sandbox=sandbox)
 market = Market(api_key, api_secret, api_passphrase, is_sandbox=sandbox)
 
@@ -28,36 +28,38 @@ while 1:
     time.sleep(60)
     # ticker of symbols
     price_a = market.get_ticker(symbol_a)
+    price_af = float(price_a['price'])
     price_b = market.get_ticker(symbol_b)
+    price_bf = float(price_b['price'])
     # position of symbols
     position_a = trade.get_position_details(symbol_a)
     position_b = trade.get_position_details(symbol_b)
     # interval of price
-    new_spread = float(price_a['price']) - float(price_b['price'])
+    new_spread = price_af - price_bf
     print('new_spread =', new_spread)
 
     if position_a['currentQty'] == position_b['currentQty'] == 0 and new_spread < (spread_mean - num_param):
-        buy_order = trade.create_limit_order(symbol_a, 'buy', leverage, size, price_a['price'] + 1)
+        buy_order = trade.create_limit_order(symbol_a, 'buy', leverage, size, price_af + 1)
         print('buy %s,order id =%s' % (symbol_a, buy_order['orderId']))
-        sell_order = trade.create_limit_order(symbol_b, 'sell', leverage, size, price_b['price'] - 1)
+        sell_order = trade.create_limit_order(symbol_b, 'sell', leverage, size, price_bf - 1)
         print('sell %s,order id =%s' % (symbol_b, sell_order['orderId']))
     elif position_a['currentQty'] == position_b['currentQty'] == 0 and new_spread > (spread_mean + num_param):
-        buy_order = trade.create_limit_order(symbol_a, 'sell', leverage, size, price_a['price'] - 1)
+        buy_order = trade.create_limit_order(symbol_a, 'sell', leverage, size, price_af - 1)
         print('sell %s,order id =%s' % (symbol_a, buy_order['orderId']))
-        sell_order = trade.create_limit_order(symbol_b, 'buy', leverage, size, price_b['price'] + 1)
+        sell_order = trade.create_limit_order(symbol_b, 'buy', leverage, size, price_bf + 1)
         print('buy %s,order id =%s' % (symbol_b, sell_order['orderId']))
     elif position_a['currentQty'] > 0 and position_b['currentQty'] < 0 and new_spread > spread_mean:
         buy_order = trade.create_limit_order(symbol_a, 'sell', position_a['realLeverage'],
-                                             position_a['currentQty'], price_a['price'] + 1)
+                                             position_a['currentQty'], price_af + 1)
         print('sell %s,order id =%s' % (symbol_a, buy_order['orderId']))
         sell_order = trade.create_limit_order(symbol_b, 'buy', position_a['realLeverage'],
-                                              position_a['currentQty'], price_b['price'] - 1)
+                                              position_a['currentQty'], price_bf - 1)
         print('buy %s,order id =%s' % (symbol_b, sell_order['orderId']))
     elif position_a['currentQty'] < 0 and position_b['currentQty'] > 0 and new_spread < spread_mean:
         buy_order = trade.create_limit_order(symbol_a, 'buy', position_a['realLeverage'],
-                                             position_a['currentQty'], price_a['price'] - 1)
+                                             position_a['currentQty'], price_af - 1)
         print('buy %s,order id =%s' % (symbol_a, buy_order['orderId']))
         sell_order = trade.create_limit_order(symbol_b, 'sell', position_a['realLeverage'],
-                                              position_a['currentQty'], price_b['price'] + 1)
+                                              position_a['currentQty'], price_bf + 1)
         print('sell %s,order id =%s' % (symbol_b, sell_order['orderId']))
 
