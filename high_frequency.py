9259b052021d2e1ed95204721a0dc74fede6db07
1bazinga25 <zyh_xg@163.com>
2020-05-06 11:14:46 +0800

    high frequency

--- high_frequency.py
+++ high_frequency.py
@@ -6,60 +6,71 @@ import time
 
 from kumex.client import Trade, Market
 
-# read configuration from json file
-with open('config.json', 'r') as file:
-    config = json.load(file)
+
+class Hf(object):
+
+    def __init__(self):
+        # read configuration from json file
+        with open('config.json', 'r') as file:
+            config = json.load(file)
+
+        self.api_key = config['api_key']
+        self.api_secret = config['api_secret']
+        self.api_passphrase = config['api_passphrase']
+        self.sandbox = config['is_sandbox']
+        self.symbol_a = config['symbol_a']
+        self.symbol_b = config['symbol_b']
+        self.spread_mean = int(config['spread_mean'])
+        self.leverage = config['leverage']
+        self.size = int(config['size'])
+        self.num_param = int(config['num_param'])
+        self.trade = Trade(self.api_key, self.api_secret, self.api_passphrase, is_sandbox=self.sandbox)
+        self.market = Market(self.api_key, self.api_secret, self.api_passphrase, is_sandbox=self.sandbox)
+
+    def get_symbol_price(self, symbol):
+        ticker = self.market.get_ticker(symbol)
+        return float(ticker['price'])
 
 
-api_key = config['api_key']
-api_secret = config['api_secret']
-api_passphrase = config['api_passphrase']
-symbol_a = config['symbol_a']
-symbol_b = config['symbol_b']
-spread_mean = int(config['spread_mean'])
-leverage = int(config['leverage'])
-size = int(config['size'])
-sandbox = config['is_sandbox']
-num_param = int(config['num_param'])
-trade = Trade(api_key, api_secret, api_passphrase, is_sandbox=sandbox)
-market = Market(api_key, api_secret, api_passphrase, is_sandbox=sandbox)
+if __name__ == '__main__':
+    hf = Hf()
+    while 1:
+        # ticker of symbols
+        price_af = hf.get_symbol_price(hf.symbol_a)
+        price_bf = hf.get_symbol_price(hf.symbol_b)
+        # position of symbols
+        position_a = hf.trade.get_position_details(hf.symbol_a)
+        position_a_qty = int(position_a['currentQty'])
+        position_b = hf.trade.get_position_details(hf.symbol_b)
+        position_b_qty = int(position_b['currentQty'])
+        # interval of price
+        new_spread = price_af - price_bf
+        print('new_spread =', new_spread)
 
-while 1:
-    time.sleep(60)
-    # ticker of symbols
-    price_a = market.get_ticker(symbol_a)
-    price_af = float(price_a['price'])
-    price_b = market.get_ticker(symbol_b)
-    price_bf = float(price_b['price'])
-    # position of symbols
-    position_a = trade.get_position_details(symbol_a)
-    position_b = trade.get_position_details(symbol_b)
-    # interval of price
-    new_spread = price_af - price_bf
-    print('new_spread =', new_spread)
+        if position_a_qty == position_b_qty == 0 and new_spread < (hf.spread_mean - hf.num_param):
+            buy_order = hf.trade.create_limit_order(hf.symbol_a, 'buy', hf.leverage, hf.size, price_af + 1)
+            print('buy %s,order id =%s' % (hf.symbol_a, buy_order['orderId']))
+            sell_order = hf.trade.create_limit_order(hf.symbol_b, 'sell', hf.leverage, hf.size, price_bf - 1)
+            print('sell %s,order id =%s' % (hf.symbol_b, sell_order['orderId']))
+        elif position_a_qty == position_b_qty == 0 and new_spread > (hf.spread_mean + hf.num_param):
+            buy_order = hf.trade.create_limit_order(hf.symbol_a, 'sell', hf.leverage, hf.size, price_af - 1)
+            print('sell %s,order id =%s' % (hf.symbol_a, buy_order['orderId']))
+            sell_order = hf.trade.create_limit_order(hf.symbol_b, 'buy', hf.leverage, hf.size, price_bf + 1)
+            print('buy %s,order id =%s' % (hf.symbol_b, sell_order['orderId']))
+        elif position_a_qty > 0 and position_b_qty < 0 and new_spread > hf.spread_mean:
+            buy_order = hf.trade.create_limit_order(hf.symbol_a, 'sell', position_a['realLeverage'],
+                                                    position_a_qty, price_af + 1)
+            print('sell %s,order id =%s' % (hf.symbol_a, buy_order['orderId']))
+            sell_order = hf.trade.create_limit_order(hf.symbol_b, 'buy', position_a['realLeverage'],
+                                                     position_a_qty, price_bf - 1)
+            print('buy %s,order id =%s' % (hf.symbol_b, sell_order['orderId']))
+        elif position_a_qty < 0 and position_b_qty > 0 and new_spread < hf.spread_mean:
+            buy_order = hf.trade.create_limit_order(hf.symbol_a, 'buy', position_a['realLeverage'],
+                                                    position_a_qty, price_af - 1)
+            print('buy %s,order id =%s' % (hf.symbol_a, buy_order['orderId']))
+            sell_order = hf.trade.create_limit_order(hf.symbol_b, 'sell', position_a['realLeverage'],
+                                                     position_a_qty, price_bf + 1)
+            print('sell %s,order id =%s' % (hf.symbol_b, sell_order['orderId']))
 
-    if position_a['currentQty'] == position_b['currentQty'] == 0 and new_spread < (spread_mean - num_param):
-        buy_order = trade.create_limit_order(symbol_a, 'buy', leverage, size, price_af + 1)
-        print('buy %s,order id =%s' % (symbol_a, buy_order['orderId']))
-        sell_order = trade.create_limit_order(symbol_b, 'sell', leverage, size, price_bf - 1)
-        print('sell %s,order id =%s' % (symbol_b, sell_order['orderId']))
-    elif position_a['currentQty'] == position_b['currentQty'] == 0 and new_spread > (spread_mean + num_param):
-        buy_order = trade.create_limit_order(symbol_a, 'sell', leverage, size, price_af - 1)
-        print('sell %s,order id =%s' % (symbol_a, buy_order['orderId']))
-        sell_order = trade.create_limit_order(symbol_b, 'buy', leverage, size, price_bf + 1)
-        print('buy %s,order id =%s' % (symbol_b, sell_order['orderId']))
-    elif position_a['currentQty'] > 0 and position_b['currentQty'] < 0 and new_spread > spread_mean:
-        buy_order = trade.create_limit_order(symbol_a, 'sell', position_a['realLeverage'],
-                                             position_a['currentQty'], price_af + 1)
-        print('sell %s,order id =%s' % (symbol_a, buy_order['orderId']))
-        sell_order = trade.create_limit_order(symbol_b, 'buy', position_a['realLeverage'],
-                                              position_a['currentQty'], price_bf - 1)
-        print('buy %s,order id =%s' % (symbol_b, sell_order['orderId']))
-    elif position_a['currentQty'] < 0 and position_b['currentQty'] > 0 and new_spread < spread_mean:
-        buy_order = trade.create_limit_order(symbol_a, 'buy', position_a['realLeverage'],
-                                             position_a['currentQty'], price_af - 1)
-        print('buy %s,order id =%s' % (symbol_a, buy_order['orderId']))
-        sell_order = trade.create_limit_order(symbol_b, 'sell', position_a['realLeverage'],
-                                              position_a['currentQty'], price_bf + 1)
-        print('sell %s,order id =%s' % (symbol_b, sell_order['orderId']))
+        time.sleep(60)
 
